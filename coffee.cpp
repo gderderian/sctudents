171704f5101d16a37ea8569a24a0efdf08b85995
Grant Derderian <gderderi@usc.edu>
2013-04-28 15:28:56 -0700

    Updating coffee item movement

--- coffee.cpp
+++ coffee.cpp
@@ -6,4 +6,30 @@ Coffee::Coffee(QPixmap *pm,int nx,int ny) : Thing(pm,nx,ny) {
 }
 
 void Coffee::move() {
+
+	int velocityX = -8;
+	int velocityY = 0;
+
+	int x_ = getX();
+	int y_ = getY();
+
+	int newX = x_ += velocityX;
+	int newY = y_ += velocityY;
+
+	if (newX >= 625 || newX <= 0){
+		hide();
+	} else {
+		setX(newX);
+	}
+ 
+ 	if (newY >= 400 || newY <= 0){
+ 		hide();
+ 	} else {
+ 		setY(newY);
+ 	}
+
+}
+
+int Coffee::getKey(){
+	return 4;
 }
--- coffee.h
+++ coffee.h
@@ -8,6 +8,7 @@ class Coffee : public Thing {
 	public:
 		Coffee(QPixmap *pm, int nx, int ny);
 		void move();
+		int getKey();
 };
 
 #endif // COFFEE_CPP
